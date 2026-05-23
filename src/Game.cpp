2ae95c94f84ee33dd7d475d30179b1aec7b745a1
tomaszgeb <vbbc8zj854@privaterelay.appleid.com>
2026-05-23 18:20:00 +0200

    dodalem kupECTS i sprawdzanie wygranej

--- src/Game.cpp
+++ src/Game.cpp
@@ -12,6 +12,19 @@ void Game::klik() {
     punkty += mocKliku;
 }
 
+// kupowanie ECTS za punkty - za 1000 pkt 1 ECTS
+bool Game::kupECTS() {
+    if(punkty < CENA_ECTS) return false;
+    punkty -= CENA_ECTS;
+    ects = ects + 1;
+    return true;
+}
+
+// wygrana = uzbieralo sie 210 ECTS
+bool Game::czyWygrana() {
+    return ects >= CEL_ECTS;
+}
+
 int Game::getPunkty() {
     return punkty;
 }
