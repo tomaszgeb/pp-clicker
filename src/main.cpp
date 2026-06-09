d59de4dbac06336e8a99efa093a4bba5d0317cff
tomaszgeb <vbbc8zj854@privaterelay.appleid.com>
2026-06-09 13:53:06 +0200

    main - podlaczam GUI Kuby + Timer Janka + Database

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,37 @@
 #include <iostream>
 #include "Game.h"
+#include "Shop.h"
+#include "GUI.h"
+#include "Database.h"
+#include "Timer.h"
+#include "RandomEvent.h"
 
-// testowy main - sprawdzam czy silnik dziala
-// docelowo Kuba podlaczy GUI na to
+// globalny wskaznik do GUI - do callbacka zdarzen losowych
+GUI *gGui = nullptr;
+
+void onZdarzenie(TypZdarzenia typ, const std::string &opis) {
+    (void)typ;
+    if(gGui) gGui->pokazPopupZdarzenia(opis);
+}
+
 int main() {
-    std::cout << "PP Clicker - test silnika" << std::endl;
+    Game gra;
+    Shop sklep(&gra);
+    RandomEvent event;
+    Database baza("pp-clicker.db");
+    Timer timer(&gra, &event);
 
-    Game g;
-    // 1500 klikow zeby kupic 1 ECTS i miec 500 pkt zapasu
-    for(int i = 0; i < 1500; i++) {
-        g.klik();
-    }
-    std::cout << "Po 1500 klikow:" << std::endl;
-    std::cout << "  punkty: " << g.getPunkty() << std::endl;
+    baza.init();
+    baza.wczytajStanGry(gra);
 
-    if(g.kupECTS()) {
-        std::cout << "Kupilem 1 ECTS" << std::endl;
-    }
-    std::cout << "  punkty po zakupie: " << g.getPunkty() << std::endl;
-    std::cout << "  ects: " << g.getEcts() << std::endl;
-    std::cout << "  wygrana? " << (g.czyWygrana() ? "tak" : "nie") << std::endl;
+    GUI gui(&gra, &sklep);
+    gGui = &gui;
+    timer.setEventCallback(onZdarzenie);
+    timer.start();
 
+    gui.uruchom();
+
+    timer.stop();
+    baza.zapiszStanGry(gra);
     return 0;
 }
