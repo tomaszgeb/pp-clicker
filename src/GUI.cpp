54c659229ebebcdaa3f86c9a4a234c0cf0fa77e6
jglinka2006-rgb <236783328+jglinka2006-rgb@users.noreply.github.com>
2026-06-10 22:00:00 +0200

    GUI - sklep, popup zdarzenia, ekran wygranej

--- src/GUI.cpp
+++ src/GUI.cpp
@@ -63,7 +63,7 @@ void GUI::obsluzZdarzenia() {
 void GUI::rysuj() {
     okno.clear(sf::Color(30, 30, 40));
 
-    // logo PP - kolo zolte
+    // logo PP
     sf::CircleShape logo(100);
     logo.setPosition(300, 150);
     logo.setFillColor(sf::Color(200, 200, 50));
@@ -74,7 +74,6 @@ void GUI::rysuj() {
     txtLogo.setFillColor(sf::Color::Black);
     okno.draw(txtLogo);
 
-    // licznik punktow i ECTS
     char buf[256];
     snprintf(buf, sizeof(buf), "Punkty: %d", gra->getPunkty());
     sf::Text txtPunkty(buf, czcionka, 24);
@@ -93,6 +92,64 @@ void GUI::rysuj() {
     txtDochod.setPosition(20, 80);
     txtDochod.setFillColor(sf::Color(180, 180, 180));
     okno.draw(txtDochod);
+
+    // sklep z prawej strony
+    for(int i = 0; i < sklep->liczbaItemow(); i++) {
+        int yi = 50 + i * 80;
+        sf::RectangleShape ramka(sf::Vector2f(180, 70));
+        ramka.setPosition(600, yi);
+        ramka.setFillColor(sf::Color(50, 50, 70));
+        ramka.setOutlineThickness(2);
+        ramka.setOutlineColor(sf::Color(100, 100, 120));
+        okno.draw(ramka);
+
+        ItemSklepu it = sklep->getItem(i);
+        sf::Text txtNazwa(it.nazwa, czcionka, 18);
+        txtNazwa.setPosition(610, yi + 5);
+        txtNazwa.setFillColor(sf::Color::White);
+        okno.draw(txtNazwa);
+
+        snprintf(buf, sizeof(buf), "%d pkt (x%d)", sklep->aktualnaCena(i), it.ilePosiadanych);
+        sf::Text txtCena(buf, czcionka, 14);
+        txtCena.setPosition(610, yi + 30);
+        txtCena.setFillColor(sf::Color(180, 180, 100));
+        okno.draw(txtCena);
+    }
+
+    // przycisk kup ECTS
+    sf::RectangleShape btnEcts(sf::Vector2f(150, 50));
+    btnEcts.setPosition(50, 500);
+    btnEcts.setFillColor(sf::Color(80, 120, 50));
+    okno.draw(btnEcts);
+    sf::Text txtBtn("Kup ECTS (1000)", czcionka, 16);
+    txtBtn.setPosition(60, 515);
+    txtBtn.setFillColor(sf::Color::White);
+    okno.draw(txtBtn);
+
+    // popup zdarzenia jak aktywny
+    if(klatkiPopupu > 0) {
+        sf::RectangleShape tlo(sf::Vector2f(600, 200));
+        tlo.setPosition(100, 200);
+        tlo.setFillColor(sf::Color(0, 0, 0, 220));
+        tlo.setOutlineThickness(3);
+        tlo.setOutlineColor(sf::Color::Red);
+        okno.draw(tlo);
+
+        sf::Text txt(aktualnyPopup, czcionka, 18);
+        txt.setPosition(120, 220);
+        txt.setFillColor(sf::Color::White);
+        okno.draw(txt);
+
+        klatkiPopupu--;
+    }
+
+    // ekran wygranej
+    if(gra->czyWygrana()) {
+        sf::Text wyg("WYGRANA! Zostales Inzynierem!", czcionka, 36);
+        wyg.setPosition(120, 300);
+        wyg.setFillColor(sf::Color::Yellow);
+        okno.draw(wyg);
+    }
 
     okno.display();
 }
